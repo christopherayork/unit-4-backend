f5233ccdd3322b7b4acbf9178ed6b466e096500b
Chris York <christopherayork1992@gmail.com>
2019-09-24 19:55:56 -0400

    seed fix

--- database/seeds/01-users.js
+++ database/seeds/01-users.js
@@ -7,7 +7,7 @@ exports.seed = function(knex) {
       return knex('users').insert([
         {id: 1, email: 'johndoe@gmail.com', password: '$2b$08$P1A.Ru7R3UIiO16AOkQvnOIDAxgYQzxa/r.wzy.tCqpXklY8J2qSq', first_name: 'John', last_name: 'Doe'},
         {id: 2, email: 'random@gmail.com', password: '$2b$08$P1A.Ru7R3UIiO16AOkQvnOIDAxgYQzxa/r.wzy.tCqpXklY8J2qSq', first_name: 'Bill', last_name: 'Johnson'},
-        {id: 3, email: 'specific@gmail.com', password: '$2b$08$P1A.Ru7R3UIiO16AOkQvnOIDAxgYQzxa/r.wzy.tCqpXklY8J2qSq', first_name: 'Ryan', last_Name: 'Hill'}
+        {id: 3, email: 'specific@gmail.com', password: '$2b$08$P1A.Ru7R3UIiO16AOkQvnOIDAxgYQzxa/r.wzy.tCqpXklY8J2qSq', first_name: 'Ryan', last_name: 'Hill'}
       ]);
     });
 };
