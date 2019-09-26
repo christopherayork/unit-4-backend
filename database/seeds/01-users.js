ddf1aae96afcf7f0bebb743fdc9b4db330bddaf3
Chris York <christopherayork1992@gmail.com>
2019-09-25 21:20:10 -0400

    Testing seeds for pg

--- database/seeds/01-users.js
+++ database/seeds/01-users.js
@@ -1,12 +1,18 @@
+// let users = [
+//   {id: 1, email: 'johndoe@gmail.com', password: '$2b$08$P1A.Ru7R3UIiO16AOkQvnOIDAxgYQzxa/r.wzy.tCqpXklY8J2qSq', first_name: 'John', last_name: 'Doe'},
+//   {id: 2, email: 'random@gmail.com', password: '$2b$08$P1A.Ru7R3UIiO16AOkQvnOIDAxgYQzxa/r.wzy.tCqpXklY8J2qSq', first_name: 'Bill', last_name: 'Johnson'},
+//   {id: 3, email: 'specific@gmail.com', password: '$2b$08$P1A.Ru7R3UIiO16AOkQvnOIDAxgYQzxa/r.wzy.tCqpXklY8J2qSq', first_name: 'Ryan', last_name: 'Hill'}
+// ];
+
 
 exports.seed = function(knex) {
   // Deletes ALL existing entries
   return knex('users').del()
     .then(function () {
       let users = [
-        {id: 1, email: 'johndoe@gmail.com', password: '$2b$08$P1A.Ru7R3UIiO16AOkQvnOIDAxgYQzxa/r.wzy.tCqpXklY8J2qSq', first_name: 'John', last_name: 'Doe'},
-        {id: 2, email: 'random@gmail.com', password: '$2b$08$P1A.Ru7R3UIiO16AOkQvnOIDAxgYQzxa/r.wzy.tCqpXklY8J2qSq', first_name: 'Bill', last_name: 'Johnson'},
-        {id: 3, email: 'specific@gmail.com', password: '$2b$08$P1A.Ru7R3UIiO16AOkQvnOIDAxgYQzxa/r.wzy.tCqpXklY8J2qSq', first_name: 'Ryan', last_name: 'Hill'}
+        {email: 'johndoe@gmail.com', password: '$2b$08$P1A.Ru7R3UIiO16AOkQvnOIDAxgYQzxa/r.wzy.tCqpXklY8J2qSq', first_name: 'John', last_name: 'Doe'},
+        {email: 'random@gmail.com', password: '$2b$08$P1A.Ru7R3UIiO16AOkQvnOIDAxgYQzxa/r.wzy.tCqpXklY8J2qSq', first_name: 'Bill', last_name: 'Johnson'},
+        {email: 'specific@gmail.com', password: '$2b$08$P1A.Ru7R3UIiO16AOkQvnOIDAxgYQzxa/r.wzy.tCqpXklY8J2qSq', first_name: 'Ryan', last_name: 'Hill'}
       ];
       // Inserts seed entries
       return Promise.all(users.map(u => knex('users').insert(u).returning('id')));
--- database/seeds/02-trips.js
+++ database/seeds/02-trips.js
@@ -1,26 +1,48 @@
+// let trips = [
+//   {id: 1, user_id: 1, location: 'Dublin, Ireland',
+//     description: 'I went to Ireland to have some beer, it was super fun!',
+//     short_desc: 'Beer in Ireland'
+//   },
+//   {id: 2, user_id: 1, location: 'Madrid, Spain',
+//     description: 'The beaches near here are great.',
+//     short_desc: 'Beaches!'
+//   },
+//   {id: 3, user_id: 2, location: 'Tokyo, Japan',
+//     description: 'Nightlife is great, change in pace.',
+//     short_desc: 'A week in Tokyo'
+//   },
+//   {id: 4, user_id: 3, location: 'Bangkok, Thailand',
+//     description: 'Huge city, great food, giant markets.',
+//     short_desc: 'Top notch spice.'
+//   },
+//   {id: 5, user_id: 3, location: 'Brussels, Belgium',
+//     description: 'Beautiful city, lots to do. Don\'t dine in if you\re on a budget.',
+//     short_desc: 'Get the chocolate.'
+//   }
+// ];
 
 exports.seed = function(knex) {
   // Deletes ALL existing entries
   return knex('trips').del()
     .then(function () {
       let trips = [
-        {id: 1, user_id: 1, location: 'Dublin, Ireland',
+        {user_id: 1, location: 'Dublin, Ireland',
           description: 'I went to Ireland to have some beer, it was super fun!',
           short_desc: 'Beer in Ireland'
         },
-        {id: 2, user_id: 1, location: 'Madrid, Spain',
+        {user_id: 1, location: 'Madrid, Spain',
           description: 'The beaches near here are great.',
           short_desc: 'Beaches!'
         },
-        {id: 3, user_id: 2, location: 'Tokyo, Japan',
+        {user_id: 2, location: 'Tokyo, Japan',
           description: 'Nightlife is great, change in pace.',
           short_desc: 'A week in Tokyo'
         },
-        {id: 4, user_id: 3, location: 'Bangkok, Thailand',
+        {user_id: 3, location: 'Bangkok, Thailand',
           description: 'Huge city, great food, giant markets.',
           short_desc: 'Top notch spice.'
         },
-        {id: 5, user_id: 3, location: 'Brussels, Belgium',
+        {user_id: 3, location: 'Brussels, Belgium',
           description: 'Beautiful city, lots to do. Don\'t dine in if you\re on a budget.',
           short_desc: 'Get the chocolate.'
         }
